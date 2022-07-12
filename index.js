c146cf9df7846ee9d1fc9f21702c88e22c40a687
Ellie <elliepark1012@gmail.com>
2022-07-12 19:50:09 -0400

    commit tree 7e0f5d92ca63135d6544b5718741c3cccc6e1863

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+return tutorials.map(tutorial => {
+const string = tutorial.split(' ')
+console.log(string)
+const word = string.map(letter => letter.charAt(0).toUpperCase()+letter.slice(1)).join(' ') 
+            return word
+})
 }
+//for (let i =0 ; i < string.length; i++){
+  //console.log(string.charAt(i))
+
+titleCased()
